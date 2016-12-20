f3ed1f65a3b920f3e2476259054ff6c29493997b
Hirotaka Suzuki <suhirotaka@gmail.com>
2016-12-21 02:16:12 +0900

    文言/デザインの修正

--- popup.js
+++ popup.js
@@ -1,4 +1,13 @@
 $(function() {
+  $('#popup-desc').text(chrome.i18n.getMessage('popup_desc'))
+  $('#link-to-option').text(chrome.i18n.getMessage('link_to_option'))
+  $('#link-to-option').click(function() {
+    chrome.runtime.openOptionsPage();
+  });
+  chrome.storage.sync.get('score', function(response) {
+    $('#your-score').text(chrome.i18n.getMessage('popup_score') + ': ' + response.score);
+  });
+
   let tableRows = $('table tr');
   let randScores = [];
   for (let i = 0; i < tableRows.length; i++) {
@@ -13,10 +22,5 @@ $(function() {
       $(tableCols[0]).text(faker.name.findName());
       $(tableCols[1]).text(randScores[i]);
     }
-  });
-  $('#popup-desc').text(chrome.i18n.getMessage('popup_desc'))
-  $('#link-to-option').text(chrome.i18n.getMessage('link_to_option'))
-  $('#link-to-option').click(function() {
-    chrome.runtime.openOptionsPage();
   });
 });
